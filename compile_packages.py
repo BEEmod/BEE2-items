dbb53ecd59ae1ad401f3fa3fe64bc0a79d7fcb42
TeamSpen210 <spencerb21@live.com>
2015-05-23 09:18:38 +1000

    Fix compile_packages to work with subfolders

--- compile_packages.py
+++ compile_packages.py
@@ -7,25 +7,31 @@ from zipfile import ZipFile
 # Users won't need them.
 SKIPPED_EXTENSIONS = ('vmx', 'log', 'bsp', 'prt', 'lin')
 
+def do_folder(path):
+    for package in os.listdir(path):
+        package_path = os.path.join(path, package)
+        if os.path.isdir(package_path):
+            if 'info.txt' in os.listdir(package_path):
+                print('| ' + package + '.zip')
+                pack_zip_path = os.path.join(zip_path, package)
+                zip = ZipFile(pack_zip_path + '.zip', 'w')
+                for base, dirs, files in os.walk(package_path):
+                    for file in files:
+                        full_path = os.path.normpath(os.path.join(base, file))
+                        rel_path = os.path.relpath(full_path, package_path)
+                        if file[-3:] in SKIPPED_EXTENSIONS:
+                            print('X   \\' + rel_path)
+                            continue
+                        print('    \\' +rel_path)
+                        
+                        zip.write(full_path, rel_path)
+                print('')
+            else:
+                do_folder(package_path)
+            
 zip_path = os.path.join(os.getcwd(), 'zips/')
 if not os.path.isdir(zip_path):
     os.makedirs(zip_path)
 
 path = os.path.join(os.getcwd(), 'packages\\')
-for package in os.listdir(path):
-    if os.path.isdir(os.path.join(path, package)):
-        print('| ' + package + '.zip')
-        package_path = os.path.join(path, package)
-        pack_zip_path = os.path.join(zip_path, package)
-        zip = ZipFile(pack_zip_path + '.zip', 'w')
-        for base, dirs, files in os.walk(package_path):
-            for file in files:
-                full_path = os.path.normpath(os.path.join(base, file))
-                rel_path = os.path.relpath(full_path, package_path)
-                if file[-3:] in SKIPPED_EXTENSIONS:
-                    print('X   \\' + rel_path)
-                    continue
-                print('    \\' +rel_path)
-                
-                zip.write(full_path, rel_path)
-        print('')
+do_folder(path)
